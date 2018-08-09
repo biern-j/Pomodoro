7ec4c9bd6a6a48a16c20ef421712cab2b7871488
jolawro <wronieckajolanta@gmail.com>
2018-08-09 21:43:11 +0200

    setIntervals

--- src/Components/Watch.jsx
+++ src/Components/Watch.jsx
@@ -3,17 +3,22 @@ import React from 'react'
 class Watch extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { time: ""};
+        this.state = { time: new Date()};
         this.currentTime = this.currentTime.bind(this);
     }
     currentTime() {
-        this.setState({ time: new Date().toLocaleDateString()});
+        this.setState({ time: new Date().toLocaleDateString() });
     }
+    componentDidMount() {
+        setInterval(this.currentTime, 1000)
+    }
+
+
     render() {
-        return(
-            <div>Now is: {this.state.time}</div>
-        );
-    }
+       return(
+           <div>Now is: {this.state.time}</div>
+       );
+   }
 }
 
 export default Watch
