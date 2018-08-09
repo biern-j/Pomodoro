a947048508b0cb8eac3b51c8f46d79d9872cef55
jolawro <wronieckajolanta@gmail.com>
2018-08-09 22:28:34 +0200

    set interval for secound

--- src/Components/Watch.jsx
+++ src/Components/Watch.jsx
@@ -3,20 +3,25 @@ import React from 'react'
 class Watch extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { time: new Date()};
-        this.currentTime = this.currentTime.bind(this);
-    }
-    currentTime() {
-        this.setState({ time: new Date().toLocaleDateString() });
-    }
-    componentDidMount() {
-        setInterval(this.currentTime, 1000)
+        this.state = { time: new Date() };
+        //this.tick = this.tick.bind(this);
     }
 
+
+    componentDidMount() {
+       this.timerID = setInterval(() => this.tick(), 1000)
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({ time: new Date() })
+    }
 
     render() {
        return(
-           <div>Now is: {this.state.time}</div>
+           <div>Now is: {this.state.time.toLocaleTimeString()}</div>
        );
    }
 }
