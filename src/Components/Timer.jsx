49fdbb9484039ca1b89c978a783b6ddf5a4994f3
jolawro <wronieckajolanta@gmail.com>
2018-08-29 19:06:06 +0200

    set counter in main page

--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -1,52 +1,20 @@
 import React from 'react'
 
-class Timer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {counter: ""};
-  }
+const style = {
+    backgroundColor: "#4CAF50", /* Green background */
+    border: "1px solid green", /* Green border */
+    color: "white", /* White text */
+    padding: "10px 24px", /* Some padding */
+    cursor: "pointer", /* Pointer/hand icon */
+    float: "left", /* Float the buttons side by side */
+};
+const Timer = ({timePeriod, description, onClick}) =>
+    <button
+        style={style}
+        onClick={(e) => onClick(e, timePeriod)}
+    >
+        {description}
+    </button>;
 
-  timer(counter){
-      this.setState({counter});
-      this.timerID =
-          setInterval(() => {
-          let value = this.state.counter;
-          console.log('value', value);
-          value = value - 1000;
-          this.setState({counter: value});
-          if (value === 0) {
-              clearInterval(this.timerID);
-              this.setState({counter: "To już"})
-          }
-
-          }, 1000);
-  }
-
-
-
-  render() {
-    const pomodoroTime = this.props.timePeriod;
-    const style = {
-      backgroundColor: "#4CAF50", /* Green background */
-      border: "1px solid green", /* Green border */
-      color: "white", /* White text */
-      padding: "10px 24px", /* Some padding */
-      cursor: "pointer", /* Pointer/hand icon */
-      float: "left", /* Float the buttons side by side */
-    };
-    return (
-        <div>
-            <button style={style} onClick={() => {
-                this.timer(pomodoroTime);
-            }}
-            >{this.props.description}
-            </button>
-             <span> YOU HAVE: {this.state.counter}</span>
-        </div>
-
-    )
-  }
-
-}
 
 export default Timer
--- src/Components/Watch.jsx
+++ src/Components/Watch.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import Timer from "./Timer";
 
+const pomodoros = [
+    {
+        timer: 5000,
+        description: "Pomodoro 5s",
+        id: 1
+    },
+    {
+        timer: 10000,
+        description: "Pomodoro 10s",
+        id: 2
+    },
+    {
+        timer: 15000,
+        description: "Pomodoro 15s",
+        id: 3
+    }
+    ];
+
+
+
 class Watch extends React.Component {
     constructor(props) {
         super(props);
-       this.state = { time: new Date()};
+        this.state = {time: new Date(), counter: ""};
     }
 
     componentDidMount() {
@@ -19,18 +39,44 @@ class Watch extends React.Component {
         this.setState({ time: new Date() })
     }
 
+    timer(e, counter){
+        e.preventDefault();
+        this.setState({counter});
+        this.timerID =
+            setInterval(() => {
+                let value = this.state.counter;
+                console.log('value', value);
+                value = value - 1000;
+                this.setState({counter: value});
+                if (value === 0) {
+                    clearInterval(this.timerID);
+                    this.setState({counter: "To już"})
+                }
+
+            }, 1000);
+    }
+
 
     render() {
+
+        const pomodoroData = pomodoros.map( item =>
+            <Timer
+                key={item.id}
+                timePeriod={item.timer}
+                description={item.description}
+                onClick={(e, counter) => this.timer(e, counter)}
+            />
+
+        );
        return(
            <div>
                <div>
                    Now is: {this.state.time.toLocaleTimeString()}
                    </div>
-
-                   <Timer timePeriod={5000} description={'Pomodoro 5s'}/>
-
-                   <Timer timePeriod={4000} description={'Pomodoro 4s'}/>
-
+               {pomodoroData}
+               <div>
+                   YOU HAVE: {this.state.counter}
+               </div>
            </div>
        );
     }
