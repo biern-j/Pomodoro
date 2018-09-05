9f99cb4fe638fab9e17352dc50ef11c1ab366026
jolawro <wronieckajolanta@gmail.com>
2018-09-05 22:47:22 +0200

    set colors

--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const style = {
-    backgroundColor: "#4CAF50", /* Green background */
-    border: "1px solid green", /* Green border */
+    backgroundColor: "#156efb", /* Green background */
+    border: "1px solid black", /* Green border */
     color: "white", /* White text */
     padding: "10px 24px", /* Some padding */
     cursor: "pointer", /* Pointer/hand icon */
