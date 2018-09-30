d14bebc5f551a2499eb5d86edec5754c5de05bc4
jolawro <wronieckajolanta@gmail.com>
2018-09-30 21:59:44 +0200

    set component for adding new pomodoro timer

--- src/Components/NewPomodoroTimer.jsx
+++ src/Components/NewPomodoroTimer.jsx
@@ -1,12 +1,48 @@
 import React from 'react';
 
-const NewPomodoroTimer = ({value, handleChange}) =>
-    <form onSubmit={this.handleSubmit}>
-        <label>
-            Name:
-            <input type="text" value={value} onChange={() => handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-    </form>;
+import Timer from "./Timer";
 
-export default NewPomodoroTimer
+
+class NewPomodoroTimer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { timer: "", description: "", timers: [] };
+    }
+
+    handleChangeTimer = (e) => this.setState({ timer: e.target.value });
+
+    handleChangeDescription = (e) => this.setState({ description: e.target.value });
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.setState({ timers: this.state.timers.concat(
+            <Timer
+                timePeriod={this.state.timer}
+                description={this.state.description}
+                onClick={ (e) => this.props.onClick(e, this.state.timer)}
+            />)});
+    };
+
+    render() {
+        return(
+            <div>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    NEW TIMER
+                <input type="text" value={this.state.timer} onChange={this.handleChangeTimer} />
+                    </label>
+                <label>
+                    NEW TIMER DESCRIPTION
+                <input type="text" value={this.state.description} onChange={this.handleChangeDescription} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+                <div>
+                    {this.state.timers}
+                </div>
+            </div>
+        );
+    }
+};
+
+export default NewPomodoroTimer;
