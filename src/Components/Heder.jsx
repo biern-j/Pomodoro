10a86434ab3d0da78d95a967daf8b7a6bd74e584
jolawro <wronieckajolanta@gmail.com>
2018-10-02 22:50:14 +0200

    delete style css

--- src/Components/Heder.jsx
+++ src/Components/Heder.jsx
@@ -1,14 +1,5 @@
 import React from 'react'
 
-const style = {
-    backgroundColor: "#d8bfd8", /* Green background */
-    border: "1px solid #d8bfd8 ", /* FireBrick  border */
-    color: "white", /* White text */
-    padding: "10px 24px", /* Some padding */
-    cursor: "pointer", /* Pointer/hand icon */
-    float: "left" /* Float the buttons side by side */
-};
-
 const Header = ({ onClick, headerDescription }) =>
-        <button style={style} onClick={() => onClick()}>{headerDescription}</button>;
+        <button onClick={() => onClick()}>{headerDescription}</button>;
  export default Header
