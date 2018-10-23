f7394f15a6a56be7368c94a6f55597d3e107c438
jolawro <wronieckajolanta@gmail.com>
2018-10-23 23:00:44 +0200

    set icon delete

--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Delete from '@material-ui/icons/Delete';
 
 
 const TimerSelector = styled(Button)`
-    display: flex;
-    position: relative;
     background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%); 
     border-radius: 3px;
     border: 0;
@@ -15,28 +13,28 @@ const TimerSelector = styled(Button)`
     box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
 `;
 
-const Container = styled.div``;
+const Container = styled.div`
+    position: relative;
+`;
 
-const Icon = {
-    position: "absolute",
-right: "-0.4rem",
-top: "-0.4rem"
-};
 
+const Icon = styled(Delete)`
+    position: absolute;
+    right: -0.4rem;
+    top: -0.4rem;
+`;
 
 const Timer = ({ id, timePeriod, onClick, onClickTimerRemover, settingPanel}) => {
-    const remove = () => settingPanel ? <FontAwesomeIcon styled={Icon} onClick={() => onClickTimerRemover(id)} icon="trash"/>: "";
+
+    const remove = () => settingPanel ? <Icon onClick={() => onClickTimerRemover(id)}/> : "";
     return (
         <Container>
             <TimerSelector onClick={(e) => onClick(e, timePeriod * 1000)}>
                 {timePeriod} seconds
             </TimerSelector>
-
-                {remove()}
-
+            {remove()}
         </Container>
     );
 };
-
 
 export default Timer
