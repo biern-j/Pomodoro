08d4a46292ed1fdf64d3ea7a22fb69ead70c3bee
jolawro <wronieckajolanta@gmail.com>
2018-10-27 23:03:13 +0200

    set clean input

--- src/Components/NewPomodoroTimer.jsx
+++ src/Components/NewPomodoroTimer.jsx
@@ -9,30 +9,30 @@ import Timer from "./Timer";
 class NewPomodoroTimer extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { timer: "", timers: [] };
+        this.state = { inputTimer: "", timer: "", timers: [] };
     }
 
-    handleChangeTimer = (e) => this.setState({ timer: e.target.value});
+    handleChangeTimer = (e) => this.setState({ timer: e.target.value });
 
     handleSubmit = (e) => {
         e.preventDefault();
-        this.setState({ timers: this.state.timers.concat(
+        this.setState({ timer: "", timers: this.state.timers.concat(
             <Timer
                 timePeriod={this.state.timer}
                 onClick={ (e) => this.props.onClick(e, this.state.timer)}
             />)});
+
     };
 
     render() {
         return(
-            <form onSubmit={(e) => this.props.onSubmit(this.handleSubmit(e))}>
+            <form onSubmit={(e) => this.props.onSubmit(this.handleSubmit(e))} type="reset">
                 <FormLabel>
                     NEW TIMER
                 <Input type="text" value={this.state.timer} onChange={(e) => {
                     this.props.handleNewTimer(e.target.value);
-                    this.handleChangeTimer(e);
-                }
-                } />
+                    this.handleChangeTimer(e);}}
+                />
                     </FormLabel>
                 <Input type="submit" value="Submit" />
             </form>
