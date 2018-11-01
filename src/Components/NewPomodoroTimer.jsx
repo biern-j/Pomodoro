72f4f0d45f9393d6a48f4791ffc26e8e732c0dac
jolawro <wronieckajolanta@gmail.com>
2018-11-01 20:57:41 +0100

    set favicon

--- src/Components/NewPomodoroTimer.jsx
+++ src/Components/NewPomodoroTimer.jsx
@@ -1,38 +1,45 @@
 import React from 'react';
 import FormLabel from '@material-ui/core/FormLabel';
 import Input from '@material-ui/core/Input';
+import  styled from 'styled-components';
 
+const Error = styled.div`
+    color: red;
+`;
 
-import TimerOption from "./TimerOption";
+const Container = styled.div``;
 
+const initialState = { minutes: "", seconds: "" };
 
 class NewPomodoroTimer extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { minutes: "00", seconds: "00" };
+        this.state = { ...initialState, error: false };
     }
-
-    handleChangeTimer = (e) => this.setState({ timer: e.target.value });
 
     handleSubmit = (e) => {
         e.preventDefault();
         const minutes = (+this.state.minutes + +this.state.seconds / 60);
-        this.props.onSubmit(minutes);
-        this.setState({ minutes: "00", seconds: "00" });
+        typeof e === "number" ? this.props.onSubmit(minutes) : this.setState({ error: true });
+        this.setState(initialState);
     };
 
     render() {
         return(
+            <Container>
             <form onSubmit={this.handleSubmit} type="reset">
                 <FormLabel>
-                    <Input type="text" inputProps={{size: 2}} value={this.state.minutes} onChange={(e) => this.setState({ minutes: e.target.value })} />
+                    <Input placeholder="00" type="text" inputProps={{size: 2}} value={this.state.minutes} onChange={(e) => this.setState({ minutes: e.target.value })} />
                     :
-                    <Input type="text" inputProps={{size: 2}} value={this.state.seconds} onChange={(e) => this.setState({ seconds: e.target.value })} />
+                    <Input placeholder="00" type="text" inputProps={{size: 2}} value={this.state.seconds} onChange={(e) => this.setState({ seconds: e.target.value })} />
                 </FormLabel>
                 <Input type="submit" value="Add" />
             </form>
+                {this.state.error ? <Error>Invalid input</Error> : "Done"}
+            </Container>
         );
     }
 };
+
 
 export default NewPomodoroTimer;
